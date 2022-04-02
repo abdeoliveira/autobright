5ebb64ee3b295882d1a67341d4433d1157a70fd3
abdeoliveira <80419593+abdeoliveira@users.noreply.github.com>
2022-04-02 09:41:04 -0300

    Update autobright.rb

--- autobright.rb
+++ autobright.rb
@@ -18,9 +18,12 @@ selfies.each.with_index do |file,m|
 end
 webcam = (avew*100).round
 #======SOME TUNINGS=======
-screen = (webcam*0.7).round
+if webcam > 70 then screen = 100 end
+if webcam > 60 and webcam <= 70 then screen = (webcam*0.85).round end
+if webcam > 30 and webcam <=60 then screen = (webcam*0.7).round end
 if webcam > 20  and webcam <= 30 then screen = webcam end
 if webcam <=20 then screen = (webcam*1.3).round end
+if webcam = 0 then screen = 1 end
 #--------------------------
 if webcam <= 10 then keyboard=50 end
 #==========================
