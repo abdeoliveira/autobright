4dae6d393e945dc2cd9c4c8bd55bb73565a7c33d
abdeoliveira <80419593+abdeoliveira@users.noreply.github.com>
2022-04-03 11:07:59 -0300

    Add files via upload

---
+++ autobright.rb
@@ -0,0 +1,50 @@
+#!/bin/ruby
+require 'mini_magick'
+#==========================================
+log_file='/home/oliveira/.autobright.log'
+snapshot_file='/home/oliveira/.selfie_autobright.jpg'
+#==========================================
+class MEASURE
+  def initialize(file)
+#------------IMPORT RAW IMAGE-------------
+    image = MiniMagick::Image.open(file)
+    pixels = image.get_pixels
+#--TAKE A SNAPSHOT AND AVERAGE BRIGHTNESS---
+    count = 0
+    white = 0
+    image.height.times do |l|
+      image.width.times do |c|
+        red     = pixels[l][c][0]
+        green   = pixels[l][c][1]
+        blue    = pixels[l][c][2]
+        white   = white + (red + green + blue)
+        count+=1
+      end
+    end
+    white = (white/765).to_f/count
+#---------------SOME METHODS-------------
+      @white = white
+      def self.white
+        @white
+      end
+#----------------------------------------
+  end
+end
+#=========================================
+keyboard=0
+`fswebcam --set 'Backlight Compensation'=1 --set Brightness=10% #{snapshot_file}`
+snap = MEASURE.new(snapshot_file)
+webcam = (snap.white*100).round
+#======SOME TUNINGS=======
+screen = webcam
+if webcam == 0 then screen = 1 end
+#--------------------------
+if webcam < 10 then keyboard=50 end
+#==========================
+`sudo brightnessctl -d 'intel_backlight' set #{screen}%`
+`sudo brightnessctl -d 'dell::kbd_backlight' set #{keyboard}%`
+#==========================
+puts output = "Measured: #{webcam}\nAdjusted: #{screen}\n" 
+File.write(log_file,output,mode:'w')
+#==========================
+
